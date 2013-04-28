de5dd71e9feba90cae4114e2c903237e7a4c56a6
Julien Michot <ju.michot@gmail.com>
2013-04-28 18:32:57 +0200

    rails new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NfcGame::Application.config.secret_token = '64d84201b7338f06196c2cb7e00b3b18bf6b3b2860d1de3bd5c1a61f404a676e132cadb59f0b836eb1426a76fff425a69c5fed898c2c5aa846ffb6da182b58f7'
